c7615a74792cc82c39f452bb78da02514dc47c80
Robert Rossmann <rr.rossmann@me.com>
2019-08-27 21:13:01 +0200

    chore: upgrade ESLint & rulesets

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,6 @@
 'use strict'
 
 module.exports = {
-  parser: 'babel-eslint',
-
   extends: [
     '@strv/node/v10',
     '@strv/node/optional',
@@ -13,19 +11,5 @@ module.exports = {
   rules: {
     // If your editor cannot show these to you, occasionally turn this off and run the linter
     'no-warning-comments': 0,
-
-    'node/no-unsupported-features/es-syntax': ['error', {
-      ignores: ['modules'],
-    }],
   },
-
-  overrides: [{
-    files: [
-      '**/*.test.mjs',
-    ],
-
-    rules: {
-      'max-classes-per-file': 'off',
-    },
-  }],
 }
