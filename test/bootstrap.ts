e8edf43c6fdecfcfb9e727a4f931e0479b775a62
Robert Rossmann <rr.rossmann@me.com>
2020-11-30 19:55:23 +0100

    test: migrate to Mocha's Root Hooks
    See: https://mochajs.org/#root-hook-plugins

--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -1,2 +1,27 @@
+import type * as mocha from 'mocha'
+import { mklistenercheck } from './utils'
+
 // eslint-disable-next-line node/no-process-env
 process.env.NODE_ENV = 'test'
+
+const mochaHooks: mocha.RootHookObject = {
+  beforeAll() {
+    // If we are running Mocha in watch mode, clear the Console before each test run so we have only
+    // one report on screen at any time. This is how the `min` reporter works by default, but we
+    // actually do want to see the full test suite log.
+    // PS. I know this is a rather dumb way to check for watch mode but hey, it works 🤷‍♂️
+    if (process.argv.includes('--watch')) {
+      process.stdout.write('\u001Bc')
+      // eslint-disable-next-line no-console
+      console.log('Terminal screen cleared in global mocha:before()\n')
+    }
+  },
+
+  afterAll() {
+    mklistenercheck(process, ['SIGINT', 'SIGTERM', 'beforeExit'])
+  },
+}
+
+export {
+  mochaHooks,
+}
--- test/global.test.ts
+++ test/global.test.ts
@@ -1,8 +1,6 @@
-import { describe, it, before, after } from 'mocha'
+import { describe, it } from 'mocha'
 import * as expect from 'expect'
 import { mklistenercheck } from './utils'
-
-const checklisteners = mklistenercheck(process, ['SIGINT', 'SIGTERM', 'beforeExit'])
 
 describe('test environment', () => {
   it('defines NODE_ENV', () => {
@@ -10,19 +8,9 @@ describe('test environment', () => {
     expect(process.env.NODE_ENV).toBe('test')
   })
 
-  it('does not have any signal listeners attached', checklisteners)
+  it('does not have any signal listeners attached', mklistenercheck(process, [
+    'SIGINT',
+    'SIGTERM',
+    'beforeExit',
+  ]))
 })
-
-before(() => {
-  // If we are running Mocha in watch mode, clear the Console before each test run so we have only
-  // one report on screen at any time. This is how the `min` reporter works by default, but we
-  // actually do want to see the full test suite log.
-  // PS. I know this is a rather dumb way to check for watch mode but hey, it works 🤷‍♂️
-  if (process.argv.includes('--watch')) {
-    process.stdout.write('\u001Bc')
-    // eslint-disable-next-line no-console
-    console.log('Terminal screen cleared in global mocha:before()\n')
-  }
-})
-
-after(checklisteners)
