009ac80a53067329a7efb8b997fbb7e681be2049
Robert Rossmann <rr.rossmann@me.com>
2020-11-30 19:52:56 +0100

    chore: remove no-longer-used Mocha reporter configuration

--- .mocharc.js
+++ .mocharc.js
@@ -1,11 +1,6 @@
 'use strict'
 
 module.exports = {
-  // Options for mocha-reporter-remote
-  // Other reporters will ignore these options.
-  reporterOption: [
-    'nostats=1',
-  ],
   colors: true,
   checkLeaks: true,
   require: [
